7c7532d12277c479e917bf430548614e680b6616
Andrei <44devtests@gmail.com>
2024-02-28 23:19:56 +0200

    navbar finished + start hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+
+import { Link } from "react-router-dom";
+
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
+import Hamburger from "hamburger-react";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [active, setActive] = useState("");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-export default Navbar
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Andrei Ion
+          </p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="sm:hidden block">
+          <Hamburger size={28} toggled={isMenuOpen} toggle={setIsMenuOpen} />
+        </div>
+      </div>
+
+      <div
+        className={`${
+          !isMenuOpen ? "-right-64" : "right-0"
+        } absolute top-20 p-6 mx-4 my-2 black-gradient min-w-36 rounded-lg transition-all duration-300`}
+      >
+        <ul className="list-none flex flex-col justify-end items-start gap-4">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[16px] font-medium cursor-pointer`}
+              onClick={() => {
+                setActive(link.title);
+                setIsMenuOpen((prev) => !prev);
+              }}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
