79038e9175ff141e255da956c2d735391e35cf1e
Andrei <44devtests@gmail.com>
2024-02-29 22:45:07 +0200

    hero section finished + changed packages version

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,23 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* <ComputersCanvas /> */}
+      <ComputersCanvas />
+
+      <div className="absolute bottom-32 xs:-bottom-10 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-9 h-16 rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
