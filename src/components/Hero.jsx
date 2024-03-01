5ac5975596019b78c9d7f9afb850fa75ed813933
Andrei <44devtests@gmail.com>
2024-03-01 23:18:41 +0200

    about section + hoc

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,17 +26,19 @@ const Hero = () => {
 
       <ComputersCanvas />
 
-      <div className="absolute bottom-32 xs:-bottom-10 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
-          <div className="w-9 h-16 rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
-              animate={{ y: [0, 24, 0] }}
+              animate={{
+                y: [0, 24, 0],
+              }}
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className="w-3 h-3 rounded-full bg-secondary"
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
           </div>
         </a>
