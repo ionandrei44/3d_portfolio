ccb56355b8a6b15915b99be027208a29da1ce4c8
Andrei <44devtests@gmail.com>
2024-03-08 22:34:30 +0200

    fix import

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
 import ParticlesContainer from "./ParticlesContainer";
 
 const Hero = () => {
